50c03655ffd0cef5cf80627845b008542d50238b
Usuario255 <vbarros.2001@hotmail.com>
2025-03-30 13:03:23 -0300

    Início do projeto

---
+++ src/main/java/br/com/calisto/calistoapi/CalistoapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.calisto.calistoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalistoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalistoapiApplication.class, args);
+	}
+
+}
